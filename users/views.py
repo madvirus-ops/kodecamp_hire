8b4390492f154d8e5ad8c6d679330237666c2ec2
madvirus-ops <edwinayabie1@gmail.com>
2022-07-25 14:41:25 +0100

    loaded static files

--- users/views.py
+++ users/views.py
@@ -7,21 +7,21 @@ from django.contrib.auth.models import User
 
 def signup(request):
     if request.method == 'POST':
-        name = request.POST.get['name']
-        email = request.POST.get['email']
-        password1 = request.POST.get['password1']
-        password2 = request.POST.get['password2']
-        phonenumber = request.POST.get['phonenumber']
-        check = request.POST.get['check']
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        phonenumber = request.POST.get('phonenumber')
+        check = request.POST.get('check')
 
         if password1 != password2:
-            messages.error('passwords do not match')
+            messages.error(request,'passwords do not match')
             return render(request, 'users/sign-up.html')
         else:
-            new_user = User.objects.create_user(name=name,email=email,password=password2)
+            new_user = User.objects.create_user(email=email,password=password2,username=name)
             new_user.save()
-            messages.success(f'Account Created For {name}')
-            return redirect('users:signup-done')
+            messages.success(request,f'Account Created For {name}')
+            return render(request, 'users/sign-up.html')
     return render(request, 'users/sign-up.html')
 
 
